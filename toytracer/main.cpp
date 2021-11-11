4e54ebcd16a179ff3a9c4d6abe3fc8ac91d277c0
Rustin Wollin <rwollin96@gmail.com>
2021-11-11 00:44:18 -0700

    chapter 6.1 ray-sphere intersection and surface normals

--- toytracer/main.cpp
+++ toytracer/main.cpp
@@ -4,16 +4,40 @@
 
 #include <iostream>
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	if (discriminant < 0) {
+		// No real solutions, missed sphere
+		return -1.0;
+	} else {
+		// 1 or 2 real solutions, return smallest t for ray r that gives a point on the sphere
+		return (-b - sqrt(discriminant)) / (2.0 * a);
+	}
+}
+
 color ray_color(const ray& r) {
+	// Test for sphere intersection
+	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0) {
+		// Hit sphere, return surface normal
+		vec3 n = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1);
+	}
+
+	// Miss, return sky gradient
 	vec3 unit_direction = unit_vector(r.direction());
-	auto t = 0.5 * (unit_direction.y() + 1.0);
+	t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
 	// Image
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width  = 300;
+	const int image_width  = 600;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	// Camera
@@ -26,6 +50,7 @@ int main() {
 	auto vertical = vec3(0, viewport_height, 0);
 	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
+	// Render to PPM file
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	// iterate over output pixels, left to right, top to bottom
